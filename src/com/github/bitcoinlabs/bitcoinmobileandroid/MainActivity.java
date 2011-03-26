010cb972850778fc47c9fa872d2c72f22a49080f
Andrew Schaaf <andrew@andrewschaaf.com>
2011-03-26 17:17:40 -0400

    {Pay,Receive} buttons

--- src/com/github/bitcoinlabs/bitcoinmobileandroid/MainActivity.java
+++ src/com/github/bitcoinlabs/bitcoinmobileandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.github.bitcoinlabs.bitcoinmobileandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button pay_button = (Button) findViewById(R.id.pay_button);
+        Button rec_button = (Button) findViewById(R.id.rec_button);
     }
 }
