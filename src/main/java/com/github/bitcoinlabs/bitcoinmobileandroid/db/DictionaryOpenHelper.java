f69e55f7c38b3cb59897cb2b6dfaca1ab1b72170
famulus <famulus.fusion@gmail.com>
2011-04-02 17:29:01 -0400

    Some hello world code for SQLite

---
+++ src/main/java/com/github/bitcoinlabs/bitcoinmobileandroid/db/DictionaryOpenHelper.java
@@ -0,0 +1,22 @@
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DictionaryOpenHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 2;
+    private static final String DICTIONARY_TABLE_NAME = "dictionary";
+
+    DictionaryOpenHelper(Context context) {
+        super(context, "test", null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create table test");
+    }
+    @Override
+    public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+}
