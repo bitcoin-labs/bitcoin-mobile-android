40c7f6b6d52fc58f0b50f31e9ea5e78b19ccf85b
Andrew Schaaf <andrew@andrewschaaf.com>
2011-04-16 10:11:46 -0400

    WalletOpenHelper

--- src/main/java/com/github/bitcoinlabs/bitcoinmobileandroid/db/WalletOpenHelper.java
+++ src/main/java/com/github/bitcoinlabs/bitcoinmobileandroid/db/WalletOpenHelper.java
@@ -4,21 +4,27 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
-public class DictionaryOpenHelper extends SQLiteOpenHelper {
+public class WalletOpenHelper extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "test2";
     private static final int DATABASE_VERSION = 2;
-    private static final String DICTIONARY_TABLE_NAME = "dictionary";
 
-    DictionaryOpenHelper(Context context) {
-        super(context, "test", null, DATABASE_VERSION);
+    WalletOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("Create table test");
+        db.execSQL(
+            "CREATE TABLE keys (" +
+            "id         INTEGER PRIMARY KEY," +
+            "address58  TEXT," +
+            "key        BLOB);");
     }
     @Override
     public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion){
-
+        // TEMP!
+        db.execSQL("DROP TABLE IF EXISTS notes");
+        onCreate(db);
     }
 }
