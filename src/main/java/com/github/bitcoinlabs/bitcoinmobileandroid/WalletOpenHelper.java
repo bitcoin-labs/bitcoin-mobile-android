bd0def9ea2022f1fb2388013f2e58c1073e8d438
Andrew Schaaf <andrew@andrewschaaf.com>
2011-05-21 21:19:06 -0400

    add TX fee

--- src/main/java/com/github/bitcoinlabs/bitcoinmobileandroid/WalletOpenHelper.java
+++ src/main/java/com/github/bitcoinlabs/bitcoinmobileandroid/WalletOpenHelper.java
@@ -133,7 +133,7 @@ public class WalletOpenHelper extends SQLiteOpenHelper {
         return satoshis;
     }
 
-    public Transaction createTransaction(long targetSatoshis, String destAddress) {
+    public Transaction createTransaction(long targetSatoshis, String destAddress, long feeSatoshis) {
         
         long satoshisGathered = 0;
         
@@ -149,14 +149,14 @@ public class WalletOpenHelper extends SQLiteOpenHelper {
         // Read outpoints
         Cursor cursor = db.query("outpoints", new String[]{"id", HASH, ADDRESS, N, SATOSHIS}, "spent = 0", null, null, null, null, null);
         cursor.moveToFirst();
-        while ((satoshisGathered < targetSatoshis) && (cursor.isAfterLast() == false)) {
+        while ((satoshisGathered < (targetSatoshis + feeSatoshis)) && (cursor.isAfterLast() == false)) {
             in_ids.add(cursor.getInt(0));
             in_hashes.add(cursor.getBlob(1));
             in_addresses.add(cursor.getString(2));
             in_indexes.add(cursor.getInt(3));
             satoshisGathered += cursor.getLong(4);
         }
-        if (satoshisGathered < targetSatoshis) {
+        if (satoshisGathered < (targetSatoshis + feeSatoshis)) {
             return null;
         }
         
@@ -188,8 +188,8 @@ public class WalletOpenHelper extends SQLiteOpenHelper {
         }
         try {
             tse.addOutput(new BigInteger("" + targetSatoshis), destAddress);
-            if (satoshisGathered < targetSatoshis) {
-                BigInteger changeSatoshis = new BigInteger("" + (satoshisGathered - targetSatoshis));
+            if (satoshisGathered < (targetSatoshis + feeSatoshis)) {
+                BigInteger changeSatoshis = new BigInteger("" + (satoshisGathered - (targetSatoshis + feeSatoshis)));
                 tse.addOutput(changeSatoshis, getUnusedAddress().toString());
             }
         }
